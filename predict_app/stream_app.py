47e9ccf2c93102d4e54b0d99047082e88469c1e3
root <root@DESKTOP-GBGLQDT.localdomain>
2024-12-02 23:12:05 +0800

    fix: Update model
    Signed-off-by: root <root@DESKTOP-GBGLQDT.localdomain>

--- predict_app/stream_app.py
+++ predict_app/stream_app.py
@@ -2,7 +2,6 @@ import streamlit as st  # 导入 Streamlit 库，用于创建 Web 应用
 import pandas as pd  # 导入 Pandas 库，用于数据处理
 import pickle  # 导入 pickle 库，用于加载已训练的模型
 import os  # 导入 os 库，用于处理文件路径
-# import shap  # 导入 SHAP 库，用于解释模型
 
 # 加载模型
 # 获取当前文件的目录
@@ -14,21 +13,16 @@ with open(model_path, 'rb') as file:
     model = pickle.load(file)  # 使用 pickle 加载模型文件
 
 # 设置 Streamlit 应用的标题
-st.title("Prediction 模型预测")
+st.title("Comprehensive mortality prediction in older patients with coronary heart disease through machine learning approaches integrating novel biomarkers: a prospective study with a 10-year follow-up")
 
-# 在侧边栏中输入特征
-st.sidebar.header("输入特征")  # 侧边栏的标题
-# 使用滑动条接收花萼长度，设置范围为 0.0 到 10.0，默认值为 5.0
-Age = st.sidebar.slider("花萼长度 (cm)", min_value=0.0, max_value=10.0, value=5.0, step=0.1)
-# 使用滑动条接收花萼宽度，设置范围为 0.0 到 10.0，默认值为 5.0
-HR = st.sidebar.slider("花萼宽度 (cm)", min_value=0.0, max_value=10.0, value=5.0, step=0.1)
-# 使用滑动条接收花瓣长度，设置范围为 0.0 到 10.0，默认值为 5.0
-Hb = st.sidebar.slider("花瓣长度 (cm)", min_value=0.0, max_value=10.0, value=5.0, step=0.1)
-# 使用滑动条接收花瓣宽度，设置范围为 0.0 到 10.0，默认值为 5.0
-HDL = st.sidebar.slider("花瓣宽度 (cm)", min_value=0.0, max_value=10.0, value=5.0, step=0.1)
-
-albumin = st.sidebar.slider("花瓣宽度 (cm)", min_value=0.0, max_value=10.0, value=5.0, step=0.1)
-fibrinogen = st.sidebar.slider("花瓣宽度 (cm)", min_value=0.0, max_value=10.0, value=5.0, step=0.1)
+st.sidebar.header("Selection Panel")  # 侧边栏的标题
+st.sidebar.subheader("Picking up parameters")
+Age = st.sidebar.slider("Age", min_value=50, max_value=110, value=85, step=1)
+HR = st.sidebar.slider("HR", min_value=30, max_value=170, value=56, step=1)
+Hb = st.sidebar.slider("Hb", min_value=50, max_value=190, value=75, step=1)
+HDL = st.sidebar.slider("HDL", min_value=0.01, max_value=3.00, value=0.52, step=0.01)
+albumin = st.sidebar.slider("Albumin", min_value=20.0, max_value=60.0, value=25.0, step=0.1)
+fibrinogen = st.sidebar.slider("Fibrinogen", min_value=0.50, max_value=10.00, value=2.18, step=0.01)
 
 # 创建输入数据框，将输入的特征整理为 DataFrame 格式
 input_data = pd.DataFrame({
@@ -36,36 +30,35 @@ input_data = pd.DataFrame({
     'HR': [HR],
     'Hb': [Hb],
     'HDL': [HDL],
-    'albumin': [albumin],
-    'fibrinogen': [fibrinogen]
+    'Albumin': [albumin],
+    'Fibrinogen': [fibrinogen]
 })
 
+time_points = [365, 3*365, 5*365, 10*365]
 
-# 添加预测按钮，用户点击后进行模型预测
-if st.button("预测"):
-    prediction = model.predict(input_data)  # 使用加载的模型进行预测
-    st.write(f"预测结果: {prediction[0]}")
+def predict_survival_probabilities(model, X, time_points):
+    surv_func = model.predict_survival_function(X)
+    survival_probabilities = pd.DataFrame()
+    for t in time_points:
+        if t in surv_func.index:
+            survival_probabilities[t] = surv_func.loc[t]
+        else:
+            closest_time = min(surv_func.index, key=lambda x: abs(x-t))
+            survival_probabilities[t] = surv_func.loc[closest_time]
+    return survival_probabilities
 
-    # # 计算 SHAP 值
-    # explainer = shap.Explainer(model)  # 或者使用 shap.TreeExplainer(model) 来计算树模型的 SHAP 值
-    # shap_values = explainer(input_data)
-    #
-    # # 提取单个样本的 SHAP 值和期望值
-    # sample_shap_values = shap_values[0]  # 提取第一个样本的 SHAP 值
-    # expected_value = explainer.expected_value[0]  # 获取对应输出的期望值
-    #
-    # # 创建 Explanation 对象
-    # explanation = shap.Explanation(
-    #     values=sample_shap_values[:, 0],  # 选择特定输出的 SHAP 值
-    #     base_values=expected_value,
-    #     data=input_data.iloc[0].values,
-    #     feature_names=input_data.columns.tolist()
-    # )
-    #
-    # # 保存为 HTML 文件
-    # shap.save_html("shap_force_plot.html", shap.plots.force(explanation, show=False))
-    #
-    # # 在 Streamlit 中显示 HTML
-    # st.subheader("模型预测的力图")
-    # with open("shap_force_plot.html") as f:
-    #     st.components.v1.html(f.read(), height=600)
+
+if st.button("Calculate"):
+    test_surv_probs = predict_survival_probabilities(model, input_data, time_points)
+
+    print(type(test_surv_probs[365]))
+    one_year = test_surv_probs[365]
+    three_year = test_surv_probs[1095]
+    five_year = test_surv_probs[1825]
+    ten_year = test_surv_probs[3650]
+    ten_year_p = 0
+    for ten in ten_year:
+        ten_year_p = ten
+    ten_rounded_num = round(ten_year_p * 100, 3)
+    st.write(f"10-year predicted survival probability: {ten_rounded_num}%")
+
